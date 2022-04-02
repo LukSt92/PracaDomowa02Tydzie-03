f54557e15c9d45aa33d294ff76a16b245fcc9ebd
LukSt92 <stach.stachu@gmail.com>
2022-04-02 20:41:54 +0200

    Add project files.

---
+++ PracaDomowa02Tydzień03/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracaDomowa02Tydzień03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int lnmb = random.Next(100);
+
+            int licznik = 1;
+
+            Console.WriteLine("System wylosował jedną liczbę z zakresu 1 - 100, proszę spróbuj którą.\nPodaj liczbę:");
+            try
+            {
+                do
+                {
+                    int nmb = int.Parse(Console.ReadLine());
+                    if (nmb > lnmb)
+                    {
+                        licznik++;
+                        Console.WriteLine("Niestety, podana przez Ciebie liczba jest za duża.\nSpróbuj jeszcze raz:");
+                    }
+
+                    else if (nmb < lnmb)
+                    {
+                        licznik++;
+                        Console.WriteLine("Niestety, podana przez Ciebie liczba jest za mała.\n Spróbuj jeszcze raz:");
+                    }
+                    else
+                    {
+                        Console.WriteLine($"Gratulacje! Udało Ci się odgadnąć wylosowaną liczbę w {licznik} próbach.");
+                        break;
+                    }
+                }
+                while (true);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Podałeś niepoprawne dane.");
+            }
+
+
+            
+        }
+    }
+}
